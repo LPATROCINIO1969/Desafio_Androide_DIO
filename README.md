1974272f6fd55931f9b293b193ca2aa1049e0ef2
LPATROCINIO1969 <lpatrocinio@terra.com.br>
2023-06-09 21:09:11 -0300

    Projeto completo, incluindo: - Texto configurado pelo STRINGS.XML - Imagem de fundo armazenada como DRAWABLE RESOURCE. - Internacionalização presente através de dois arquivos STRINGS.XML (português e inglês).

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Desafio_Androide_DIO
-Projeto relativo ao desafio de criação de app em ANDROID usando Kotlin (DIO)
+Projeto relativo ao desafio de criação de app em ANDROID usando Kotlin (DIO).
--- app/src/main/java/br/com/luciopp/desafio_android_dio/MainActivity.kt
+++ app/src/main/java/br/com/luciopp/desafio_android_dio/MainActivity.kt
@@ -3,13 +3,29 @@ package br.com.luciopp.desafio_android_dio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.res.TypedArrayUtils.getText
 import br.com.luciopp.desafio_android_dio.ui.theme.Desafio_Android_DIOTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +38,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(stringResource(R.string.saudacao_text), stringResource(R.string.identificacao_text))
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(message: String, name: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable._eb61c81f031514445061c98b2e18e16)
+    Box {
+        Image(painter = image,contentDescription = null,
+            modifier.fillMaxSize(), contentScale = ContentScale.Crop)
+        Text(
+                text = "$message $name",
+                fontSize = 28.sp,
+                color = Color.Blue,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all=16.dp)
+                    .wrapContentWidth(align = Alignment.CenterHorizontally)
+                    .wrapContentHeight(align = Alignment.CenterVertically)
+            )
+
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Desafio_Android_DIOTheme {
-        Greeting("Android")
+        Greeting(stringResource(R.string.saudacao_text), stringResource(R.string.identificacao_text))
     }
 }
